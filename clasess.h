3905d19841d9689bf731412a8143059736a76f35
Kevin Ismael Valdez Mendoza <86176535+Kevinvaldez@users.noreply.github.com>
2021-07-16 00:55:26 -0500

    este el archivo para crear las funciones
    aqui podran editar su funcion

---
+++ clasess.h
@@ -0,0 +1,43 @@
+
+ 
+
+class persona{
+
+private:
+int numero de cedula;
+int fecha de nacimiento;
+string nombres;
+int peso;	
+float estatura;
+public:
+	persona(int, int, string, int, float,);
+	void mostrar datos personales();
+	void mostrar edad();
+	void mostrar masa corporal();
+	void guardar datos();
+	void recuperar datos();
+	
+
+
+persona::persona(int -numero de cedula,int -fecha de nacimiento, string -nombres, int peso, float -estatura){
+	numero de cedula = -numero de cedula;
+	fecha de nacimiento = -fecha de nacimiento;
+	nombres = -nombres;
+	peso = -peso;
+	estatura = -estatura;
+	
+}
+
+void persona:: mostrar datos personales();{
+cout<< soy 
+
+}
+
+int main(){
+	
+	
+	system ("pause");
+	
+	return 0;
+}
+};
